2d6c8735251024f9ca67fc61d939c8c8f7acd36a
Nathan Lubchenco <nathan@simpleenergy.com>
2014-06-04 08:06:10 -0600

    finished simulation of market and inflation
    also added aggregated simulated retirement type and stubbed function
    for eventual end results

--- simulator/app/core/simulator.scala
+++ simulator/app/core/simulator.scala
@@ -20,12 +20,28 @@ class simulator {
 
     val rng = new Random
 
-    range.toList.map(x => SimulatedMarketReturn(genGaussian(earningsStdDev, earningsMean, rng)))
+    range.toList map { x =>
+      SimulatedMarketReturn(genGaussian(earningsStdDev, earningsMean, rng), genGaussian(yieldsStdDev, yieldsMean, rng))
+    }
   }
 
-  def simulateInflation(n: Int, data: List[HistoricalInflation]): List[SimulatedInflation] = ???
+  def simulateInflation(n: Int, data: List[HistoricalInflation]): List[SimulatedInflation] = {
+    val range = 1 to n
+
+    val inflation = data.map(_.inflation)
+    val inflationStdDev = stdDev(inflation)
+    val inflationMean = inflation.sum / inflation.size.toDouble
+
+    val rng = new Random
+
+    range.toList map { x =>
+      SimulatedInflation(genGaussian(inflationStdDev, inflationMean, rng))
+    }
+  }
 
   def simulateEarlyRetirement(params: RetirementParameters, market: List[SimulatedMarketReturn], inflation: List[SimulatedInflation]): SimulatedRetirement = ???
+
+  def aggregatedSimulatedRetirements(sims: List[SimulatedRetirement], runs: Int): AggregatedSimulatedRetirements = ???
 
   def stdDev(data: List[Double]) = {
     val mean = data.sum / data.length
@@ -48,9 +64,9 @@ class simulator {
 }
 
 case class HistoricalMarketReturn(earningsPerc: Double, yieldPerc: Double)
-case class SimulatedMarketReturn(d: Double) extends AnyVal
+case class SimulatedMarketReturn(earningsPerc: Double, yieldPerc: Double)
 case class HistoricalInflation(inflation: Double) extends AnyVal
-case class SimulatedInflation(d: Double) extends AnyVal
+case class SimulatedInflation(inflation: Double) extends AnyVal
 
 case class InitialCapital(d: Double) extends AnyVal
 case class EstimatedMonthlyExpenses(d: Double) extends AnyVal
@@ -66,3 +82,4 @@ case class SimulatedCapital(d: Double) extends AnyVal
 case class Failure(b: Boolean) extends AnyVal
 
 case class SimulatedRetirement(simulatedCapital: List[SimulatedCapital], failure: Failure)
+case class AggregatedSimulatedRetirements(failurePerc: Double, averageRemainingCapital: Double, maxRemainingCapital: Double, minRemainingCapital: Double, rawData: List[SimulatedRetirement])
